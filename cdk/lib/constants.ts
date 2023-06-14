94a3979393f264039c4d91b8873dbf257db11dbd
Marc Brophy <aldrichbrophy@gmail.com>
2023-06-14 00:54:48 -0700

    updated domain stack region and node version

--- cdk/lib/constants.ts
+++ cdk/lib/constants.ts
@@ -3,10 +3,10 @@ export const constants = {
   SERVICE_NAME: 'minecraft-server',
   MC_SERVER_CONTAINER_NAME: 'minecraft-server',
   WATCHDOG_SERVER_CONTAINER_NAME: 'minecraft-ecsfargate-watchdog',
-  DOMAIN_STACK_REGION: 'us-east-1',
+  DOMAIN_STACK_REGION: 'us-west-2',
   ECS_VOLUME_NAME: 'data',
   HOSTED_ZONE_SSM_PARAMETER: 'MinecraftHostedZoneID',
   LAUNCHER_LAMBDA_ARN_SSM_PARAMETER: 'LauncherLambdaRoleArn',
-  JAVA_EDITION_DOCKER_IMAGE: 'itzg/minecraft-server',
+  JAVA_EDITION_DOCKER_IMAGE: 'itzg/minecraft-server', // Update to custom Docker image.
   BEDROCK_EDITION_DOCKER_IMAGE: 'itzg/minecraft-bedrock-server',
 }
